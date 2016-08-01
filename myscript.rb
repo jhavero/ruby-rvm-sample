89d28fae38b3ca38115ecf0a05605a2dfce6ba8d
Alex Raguerp <jenkins@precise64.(none)>
2016-08-01 05:35:24 +0000

    print gem version

--- myscript.rb
+++ myscript.rb
@@ -9,3 +9,6 @@ logger.level = Logger::WARN
 logger.debug("Created logger")
 logger.info("Program started")
 logger.warn("Nothing to do!")
+
+puts "logger version"
+puts Gem.loaded_specs["logger"].version
