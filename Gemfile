d5dee0f3df3e8be4ea1c2b14596e5f51a86da1b7
Alex Raguerp <jenkins@precise64.(none)>
2016-08-01 03:20:11 +0000

    init

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+# A sample Gemfile
+source "https://rubygems.org"
+gem 'logger', '= 1.2.8'
+
+# gem "rails"
---
+++ myscript.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+require 'logger'
+
+logger = Logger.new(STDOUT)
+logger.level = Logger::WARN
+
+logger.debug("Created logger")
+logger.info("Program started")
+logger.warn("Nothing to do!")
